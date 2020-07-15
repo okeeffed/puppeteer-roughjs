953cd4de116f60890e7ac7430304fe6060c0bfcd
Dennis O'Keeffe <hello@dennisokeeffe.com>
2020-07-16 07:37:58 +1000

    docs: Updating comments + README

--- README.md
+++ README.md
@@ -22,14 +22,24 @@ rough - Quick RoughJS diagram generator
 Examples
 ---
 
-> node index.js "Testing this this{} | Another one | Third one {fill: 'red'}"
-> node index.js "Testing this this{fill: 'blue', fillWeight: 3, hachureGap: 8} | Another one | Third one{fill: 'red'} | Final square {fillStyle: 'solid'}"
-> node index.js "This is a very long sentence that will resize the box | This box will keep that width {fill: 'yellow', hachureGap: 3} "
+> rough "Testing this this{} | Another one | Third one {fill: 'red'}"
+> rough "Testing this this{fill: 'blue', fillWeight: 3, hachureGap: 8} | Another one | Third one{fill: 'red'} | Final square {fillStyle: 'solid'}"
+> rough "This is a very long sentence that will resize the box | This box will keep that width {fill: 'yellow', hachureGap: 3} "
 
-Roughjs Options
+Rough Options
 ---
 hachureAngle: 60 // angle of hachure
 hachureGap: 8 // gap between hachure lines
 fillStyle: 'solid' // solid fill
 fillWeight: 3 // thicker lines for hachure
 ```
+
+## Resources
+
+1. [Open Sans](https://fonts.google.com/specimen/Open+Sans?sidebar.open&selection.family=Open+Sans)
+2. [Text onto Canvas](https://www.w3schools.com/graphics/canvas_text.asp)
+3. [RoughJS](https://roughjs.com/)
+4. [Puppeteer](https://github.com/puppeteer/puppeteer)
+5. [Yargs Parser](https://github.com/yargs/yargs-parser)
+6. [Screenshots with Puppeteer - Blog Post](https://blog.dennisokeeffe.com/blog/2020-07-01-screenshot-anything-with-puppeteer/)
+7. [Intro Yargs Parser - Blog Post](https://blog.dennisokeeffe.com/blog/yargs-parser/)
--- index.js
+++ index.js
@@ -15,11 +15,19 @@ rough - Quick RoughJS diagram generator
 Examples
 ---
 
-> node index.js "Testing this this{} | Another one | Third one {fill: 'red'}"
-> node index.js "Testing this this{fill: 'blue', fillWeight: 3, hachureGap: 8} | Another one | Third one{fill: 'red'} | Final square {fillStyle: 'solid'}"
-> node index.js "This is a very long sentence that will resize the box | This box will keep that width {fill: 'yellow', hachureGap: 3} "
+# Basic with three boxes
+rough "First box, no options {} | Second | Third with red fill {fill: 'red'}"
 
-Roughjs Options
+# More Complex Config
+rough "Testing this this{fill: 'blue', fillWeight: 3, hachureGap: 8} | Another one | Third one{fill: 'red'} | Final square {fillStyle: 'solid'}"
+
+# Example with the resizing
+rough "This is a very long sentence that will resize the box | This box will keep that width {fill: 'yellow', hachureGap: 3} "
+
+# Solid Fill
+rough "Lorem Ipsum | Run out of things to say {fill: 'yellow', fillStyle: 'solid'}"
+
+Rough Options
 ---
 
 hachureAngle: 60 // angle of hachure
@@ -28,20 +36,28 @@ fillStyle: 'solid' // solid fill
 fillWeight: 3 // thicker lines for hachure
 `;
 
-if (argv.help) {
+// Check for help or any argument length.
+// If not give, log out help and exit.
+if (argv.help || !argv._.length) {
   console.log(help);
+  process.exit();
 }
 
-// allocated size per character
+/** Constants */
+// Allocated size per character
 const PER_CHAR = 8;
 const HEIGHT = 80;
+// Displacement between each box
 const DISPLACEMENT = 24;
+// Desired padding around the box from the edges
+const PADDING = 10;
 
-// getting arguments
+/** Fetching arguments */
 const [args] = argv._;
 const shapes = args.split('|');
-
-const PADDING = 10;
+// Used later as a "global" store for
+// parsed text and options.
+const shapeObjs = [];
 
 /**
  * Add a rectangle to the canvas
@@ -83,8 +99,7 @@ const addLine = (x1, y1, x2, y2) => `rc.line(${x1}, ${y1}, ${x2}, ${y2});`;
 //   `addTextToCircle(${text}, ${centerX}, ${centerY})`;
 
 const generateString = () => {
-  let size = 0;
-  const shapeObjs = [];
+  let len = 0;
 
   for (const shape of shapes) {
     const hasOptions = shape.indexOf('{');
@@ -96,8 +111,8 @@ const generateString = () => {
         text,
       });
 
-      if (text.length > size) {
-        size = text.length;
+      if (text.length > len) {
+        len = text.length;
       }
     } else {
       // renaming for understanding
@@ -106,24 +121,23 @@ const generateString = () => {
         text,
       });
 
-      if (text.length > size) {
-        size = text.length;
+      if (text.length > len) {
+        len = text.length;
       }
     }
   }
 
   let js = '';
 
-  const WIDTH = size * PER_CHAR + DISPLACEMENT;
+  const WIDTH = len * PER_CHAR + DISPLACEMENT;
 
   shapeObjs.map((obj, index) => {
     const startY = index * (HEIGHT + DISPLACEMENT);
-    console.log('startY', startY);
+
     js += `\n${addRectangle(0, startY, WIDTH, HEIGHT, obj.options)}`;
     js += `\n${addTextToRectangle(obj.text, 0, startY, WIDTH, HEIGHT)}`;
 
     if (index > 0) {
-      console.log('HERE');
       js += `\n${addLine(WIDTH / 2, startY - DISPLACEMENT, WIDTH / 2, startY)}`;
     }
   });
@@ -154,31 +168,40 @@ const rc = rough.canvas(document.getElementById('canvas'));
 ${generateString()}
 `;
 
-const calcWidth = () => {
-  let size = 0;
+/**
+ * Based on input, it will figure out the desired
+ * width of the boxes. We essentially take the text
+ * between the pipes, abstract any config, trim it
+ * and then return the size to be used for width
+ * calculation.
+ *
+ * @returns {number} Length of largest string
+ */
+const calcLongestTextSize = () => {
+  let len = 0;
 
   for (const shape of shapes) {
     const hasOptions = shape.indexOf('{');
     if (hasOptions > 0) {
       const text = shape.substr(0, hasOptions).trim();
 
-      if (text.length > size) {
-        size = text.length;
+      if (text.length > len) {
+        len = text.length;
       }
     } else {
       // renaming for understanding
       const text = shape.trim();
 
-      if (text.length > size) {
-        size = text.length;
+      if (text.length > len) {
+        len = text.length;
       }
     }
   }
 
-  return size;
+  return len;
 };
 
-const CALCULATED_WIDTH = calcWidth() * PER_CHAR + DISPLACEMENT;
+const CALCULATED_WIDTH = calcLongestTextSize() * PER_CHAR + DISPLACEMENT;
 const CALCULATED_HEIGHT = shapes.length * (DISPLACEMENT + HEIGHT) - PADDING * 2;
 
 const html = `<!DOCTYPE html>
@@ -196,11 +219,17 @@ const html = `<!DOCTYPE html>
   </script>
 </html>`;
 
-console.log(html);
+if (argv.v) {
+  console.log(html);
+}
 
 const main = async () => {
   let browser;
   try {
+    // Log out global config taken from command-line
+    console.log('\nBeginning Roughjs canvas generation...');
+    console.log('\nConfig:', shapeObjs);
+
     // Launch Puppeteer and setup a new page
     browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -212,7 +241,7 @@ const main = async () => {
     // for at least 500 ms."
     await page.goto(`data:text/html,${html}`, { waitUntil: 'networkidle0' });
 
-    // Wait for the <span id="mykatex" /> element to be visible
+    // Wait for the <span id="canvas" /> element to be visible
     // and assign it to "element".
     const element = await page.$('#canvas');
 
@@ -224,6 +253,7 @@ const main = async () => {
     console.error(err);
   } finally {
     await browser.close();
+    console.log('Completed!');
   }
 };
 
